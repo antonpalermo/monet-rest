8549fa2fd8c114be9a8176d1c59ccc1086f067ef
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-10-19 14:43:19 +0800

    added create entry endpoint

--- routes/entries.route.ts
+++ routes/entries.route.ts
@@ -1,4 +1,10 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import { entry } from "../database/schemas/entry";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
@@ -10,13 +16,34 @@ app
     });
   })
   // creates a new entry in the ledger
-  .post("/", c => {
-    return c.json(
-      {
-        message: "ok"
-      },
-      201
-    );
+  .post("/create", async ctx => {
+    const sql = neon(ctx.env.DATABASE_URL);
+    const db = drizzle(sql);
+
+    const body = await ctx.req.json();
+
+    try {
+      const result = await db
+        .insert(entry)
+        .values({
+          description: body.description,
+          amount: body.amount
+        })
+        .returning();
+
+      return ctx.json(
+        {
+          data: result[0],
+          message: "entry successfully created"
+        },
+        201
+      );
+    } catch (error) {
+      throw new HTTPException(500, {
+        message: "internal server error",
+        cause: error
+      });
+    }
   })
   .get("/:id", c => {
     return c.json({
