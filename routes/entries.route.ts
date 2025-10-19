a72a9a932915bbdaa9432ed9829fbe00375a440d
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-10-19 15:41:20 +0800

    added get all entry endpoint

--- routes/entries.route.ts
+++ routes/entries.route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 import { neon } from "@neondatabase/serverless";
+import { desc, gt, sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { entry } from "../database/schemas/entry";
@@ -10,15 +11,33 @@ const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app
   // get all entries
-  .get("/", c => {
-    return c.json({
-      message: "ok"
-    });
+  .get("/", async ctx => {
+    const psql = neon(ctx.env.DATABASE_URL);
+    const db = drizzle(psql);
+
+    try {
+      const dateFilter = sql`NOW() - INTERVAL '30days'`;
+      const result = await db
+        .select()
+        .from(entry)
+        .where(gt(entry.dateCreated, dateFilter))
+        .orderBy(desc(entry.dateCreated));
+
+      return ctx.json({
+        data: result,
+        message: "successfully get all entries"
+      });
+    } catch (error) {
+      throw new HTTPException(500, {
+        message: "internal server error",
+        cause: error
+      });
+    }
   })
   // creates a new entry in the ledger
   .post("/create", async ctx => {
-    const sql = neon(ctx.env.DATABASE_URL);
-    const db = drizzle(sql);
+    const psql = neon(ctx.env.DATABASE_URL);
+    const db = drizzle(psql);
 
     const body = await ctx.req.json();
 
