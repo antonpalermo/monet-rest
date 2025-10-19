75dc0caf5dfd2dfb4910dfb6984468e79caf9a8a
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-10-19 23:39:54 +0800

    added endpoint to ge specific entry

--- routes/entries.route.ts
+++ routes/entries.route.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 import { neon } from "@neondatabase/serverless";
-import { desc, gt, sql } from "drizzle-orm";
+import { desc, eq, gt, sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { entry } from "../database/schemas/entry";
@@ -64,10 +64,29 @@ app
       });
     }
   })
-  .get("/:id", c => {
-    return c.json({
-      message: "ok"
-    });
+  .get("/:id", async ctx => {
+    const psql = neon(ctx.env.DATABASE_URL);
+    const db = drizzle(psql);
+
+    const id = ctx.req.param("id");
+
+    try {
+      const result = await db.select().from(entry).where(eq(entry.id, id));
+
+      if (!result.length) {
+        return ctx.notFound();
+      }
+
+      return ctx.json({
+        data: result,
+        message: "successfully get all entries"
+      });
+    } catch (error) {
+      throw new HTTPException(500, {
+        message: "internal server error",
+        cause: error
+      });
+    }
   })
   .patch("/:id", c => {
     return c.json({
