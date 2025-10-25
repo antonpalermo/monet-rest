b24047ebbb27782086028d5b778676635a6ead3b
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-10-25 23:40:40 +0800

    added create ledger route

--- routes/ledgers.routes.ts
+++ routes/ledgers.routes.ts
@@ -1,4 +1,8 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { Hono } from "hono";
+import { ledger } from "../database/schemas/ledger";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
@@ -9,9 +13,30 @@ app
     });
   })
   .post("/create", async ctx => {
-    return ctx.json({
-      message: "ok"
-    });
+    const psql = neon(ctx.env.DATABASE_URL);
+    const db = drizzle(psql);
+
+    const body = await ctx.req.json();
+
+    try {
+      const result = await db
+        .insert(ledger)
+        .values({ name: body.name })
+        .returning();
+
+      return ctx.json(
+        {
+          data: result[0],
+          message: "ledger successfully created"
+        },
+        201
+      );
+    } catch (error) {
+      throw new HTTPException(500, {
+        message: "internal server error",
+        cause: error
+      });
+    }
   })
   .get("/:id", async ctx => {
     return ctx.json({
