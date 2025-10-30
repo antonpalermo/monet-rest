74a79dc0036a98d803f9326ea16e72e5e12452c7
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-10-30 22:59:41 +0800

    moved database init on hono context

--- server.ts
+++ server.ts
@@ -2,13 +2,32 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { secureHeaders } from "hono/secure-headers";
 
+import { drizzle, type NeonHttpDatabase } from "drizzle-orm/neon-http";
+
 import ledgerRoutes from "./routes/ledgers.routes";
 import entriesRoutes from "./routes/entries.route";
+import { neon } from "@neondatabase/serverless";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>().basePath("/api");
+export type AppEnv = {
+  Bindings: CloudflareBindings;
+  Variables: {
+    db: NeonHttpDatabase;
+  };
+};
+
+const app = new Hono<AppEnv>().basePath("/api");
 
 app.use(logger());
 app.use(secureHeaders());
+
+app.use(async (ctx, next) => {
+  const neonClient = neon(ctx.env.DATABASE_URL);
+  const db = drizzle(neonClient);
+  // set db context.
+  ctx.set("db", db);
+  // proceed with the next step.
+  await next();
+});
 
 app.route("/ledgers", ledgerRoutes);
 app.route("/entries", entriesRoutes);
