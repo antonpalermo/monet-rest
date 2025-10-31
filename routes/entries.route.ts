dde97ca212245c7a5cd2a3ec792926d07bebaeb9
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-10-31 22:54:11 +0800

    refactor get all entries route

--- routes/entries.route.ts
+++ routes/entries.route.ts
@@ -1,154 +1,25 @@
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
+import { desc, gt, sql } from "drizzle-orm";
 
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import { desc, eq, gt, sql } from "drizzle-orm";
-
+import { AppEnv } from "../server";
 import { entry } from "../database/schemas/entry";
-import { validate } from "../libs/validation";
-import { paramSchema, entrySchema } from "../libs/schemas";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+const app = new Hono<AppEnv>();
 
-app
-  // get all entries
-  .get("/", async ctx => {
-    const psql = neon(ctx.env.DATABASE_URL);
-    const db = drizzle(psql);
-
-    try {
-      const dateFilter = sql`NOW() - INTERVAL '30days'`;
-      const result = await db
-        .select()
-        .from(entry)
-        .where(gt(entry.dateCreated, dateFilter))
-        .orderBy(desc(entry.dateCreated));
-
-      return ctx.json({
-        data: result,
-        message: "successfully get all entries"
-      });
-    } catch (error) {
-      throw new HTTPException(500, {
-        message: "internal server error",
-        cause: error
-      });
-    }
-  })
-  // creates a new entry in the ledger
-  .post("/create", validate("json", entrySchema.strict()), async ctx => {
-    const psql = neon(ctx.env.DATABASE_URL);
-    const db = drizzle(psql);
-
-    const body = await ctx.req.json();
-
-    try {
-      const result = await db
-        .insert(entry)
-        .values({
-          description: body.description,
-          amount: body.amount,
-          ledgerId: body.ledgerId
-        })
-        .returning();
-
-      return ctx.json(
-        {
-          data: result[0],
-          message: "entry successfully created"
-        },
-        201
-      );
-    } catch (error) {
-      throw new HTTPException(500, {
-        message: "internal server error",
-        cause: error
-      });
-    }
-  })
-  .get("/:id", validate("param", paramSchema), async ctx => {
-    const psql = neon(ctx.env.DATABASE_URL);
-    const db = drizzle(psql);
-
-    const { id } = ctx.req.param();
-
-    try {
-      const result = await db.select().from(entry).where(eq(entry.id, id));
-
-      if (!result.length) {
-        return ctx.notFound();
-      }
-
-      return ctx.json({
-        data: result[0],
-        message: "successfully get all entries"
-      });
-    } catch (error) {
-      throw new HTTPException(500, {
-        message: "internal server error",
-        cause: error
-      });
-    }
-  })
-  .patch(
-    "/:id",
-    validate("param", paramSchema),
-    validate("json", entrySchema.partial().strict()),
-    async ctx => {
-      const psql = neon(ctx.env.DATABASE_URL);
-      const db = drizzle(psql);
-
-      const { id } = ctx.req.param();
-
-      const body = await ctx.req.json();
-
-      try {
-        const result = await db
-          .update(entry)
-          .set({ ...body })
-          .where(eq(entry.id, id))
-          .returning();
-
-        if (!result.length) {
-          return ctx.notFound();
-        }
-
-        return ctx.json({
-          data: result[0],
-          message: "successfully updated"
-        });
-      } catch (error) {
-        throw new HTTPException(500, {
-          message: "internal server error",
-          cause: error
-        });
-      }
-    }
-  )
-  .delete("/:id", validate("param", paramSchema), async ctx => {
-    const psql = neon(ctx.env.DATABASE_URL);
-    const db = drizzle(psql);
-
-    const { id } = ctx.req.param();
-
-    try {
-      const result = await db.delete(entry).where(eq(entry.id, id)).returning();
-
-      if (!result.length) {
-        return ctx.notFound();
-      }
-
-      return ctx.json({
-        data: result[0],
-        message: `${id} successfully removed`
-      });
-    } catch (error) {
-      throw new HTTPException(500, {
-        message: "internal server error",
-        cause: error
-      });
-    }
+app.get("/", async ctx => {
+  const db = ctx.get("db");
+  // return entries for the past 30 days
+  const dateFilter = sql`NOW() - INTERVAL '30days'`;
+  // select all entries.
+  const entries = await db
+    .select()
+    .from(entry)
+    .where(gt(entry.dateCreated, dateFilter))
+    .orderBy(desc(entry.dateCreated));
+  // return all selected entries.
+  return ctx.json({
+    data: entries,
+    success: true,
+    message: "entries successfully fetched"
   });
-
-export default app;
+});
