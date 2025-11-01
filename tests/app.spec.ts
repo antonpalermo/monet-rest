8de55707a21adb0d439a9cf26e5f0245a1bdde37
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-11-02 00:24:52 +0800

    update test spect setup for vitest

---
+++ tests/app.spec.ts
@@ -0,0 +1,10 @@
+import { env } from "cloudflare:test";
+import app from "../server";
+
+describe("app specs", () => {
+  it("api should return 200 status", async () => {
+    const request = await app.request("/api", {}, env);
+    expect(request.status).toBe(200);
+    expect(await request.text()).toMatch("Hello Hono!");
+  });
+});
--- tests/entries.spec.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "cloudflare:test";
-import app from "../server";
-
-describe("entries", () => {
-  it("get all entries should return 200 ok and match retuned object", async () => {
-    const request = await app.request("/api/entries", {}, env);
-    expect(request.status).toBe(200);
-  });
-});
