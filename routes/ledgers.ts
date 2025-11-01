11084652bee8a6ac2fb0e99836f5a7aa2d575733
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-11-01 14:43:13 +0800

    refactor create ledger route

--- routes/ledgers.ts
+++ routes/ledgers.ts
@@ -13,6 +13,26 @@ import { paramSchema, ledgerSchema } from "../libs/schemas";
 
 const app = new Hono<AppEnv>();
 
+app.post("/create", validate("json", ledgerSchema.strict()), async ctx => {
+  const db = ctx.get("db");
+
+  const body = await ctx.req.json();
+
+  const createdLedger = await db
+    .insert(ledger)
+    .values({ name: body.name })
+    .returning();
+
+  return ctx.json(
+    {
+      data: createdLedger[0],
+      success: true,
+      message: "ledger successfully created"
+    },
+    201
+  );
+});
+
 app.get("/", async ctx => {
   const db = ctx.get("db");
 
@@ -28,32 +48,6 @@ app.get("/", async ctx => {
 });
 
 app
-  .post("/create", validate("json", ledgerSchema.strict()), async ctx => {
-    const psql = neon(ctx.env.DATABASE_URL);
-    const db = drizzle(psql);
-
-    const body = await ctx.req.json();
-
-    try {
-      const result = await db
-        .insert(ledger)
-        .values({ name: body.name })
-        .returning();
-
-      return ctx.json(
-        {
-          data: result[0],
-          message: "ledger successfully created"
-        },
-        201
-      );
-    } catch (error) {
-      throw new HTTPException(500, {
-        message: "internal server error",
-        cause: error
-      });
-    }
-  })
   .get("/:id", validate("param", paramSchema), async ctx => {
     const psql = neon(ctx.env.DATABASE_URL);
     const db = drizzle(psql);
