56510a874b81230c1cd599f0ab4b72fdd4a87c54
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-11-04 23:15:49 +0800

    added configs

---
+++ libs/auth.ts
@@ -0,0 +1,18 @@
+import { neon } from "@neondatabase/serverless";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { drizzle } from "drizzle-orm/neon-http";
+
+export const auth = (
+  env: CloudflareBindings
+): ReturnType<typeof betterAuth> => {
+  const psql = neon(env.DATABASE_URL);
+  const db = drizzle(psql);
+
+  return betterAuth({
+    appName: "Monet",
+    database: drizzleAdapter(db, { provider: "pg" }),
+    baseURL: env.BETTER_AUTH_URL,
+    secret: env.BETTER_AUTH_SECRET
+  });
+};
