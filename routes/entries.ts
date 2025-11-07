f7e59341097a55d54cddc53514ae77f0e3347ea0
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-11-07 23:09:47 +0800

    fix build errors

--- routes/entries.ts
+++ routes/entries.ts
@@ -3,7 +3,7 @@ import { desc, eq, gt, sql } from "drizzle-orm";
 
 import { entry } from "../database/schemas/entry";
 
-import { AppEnv } from "../workers/server";
+import type { AppEnv } from "../workers/server";
 import { validate } from "../libs/validation";
 import { entrySchema, paramSchema } from "../libs/schemas";
 
