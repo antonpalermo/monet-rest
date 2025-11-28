ac435064ab8aaf6cf8fb36f44700a6322a308ffb
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-11-28 23:10:33 +0800

    update absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 
+import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
@@ -16,5 +17,16 @@ export default defineConfig({
     react(),
     tailwind(),
     cloudflare()
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@client": path.resolve(__dirname, "./client"),
+      "@components": path.resolve(__dirname, "./client/components"),
+      "@lib": path.resolve(__dirname, "./client/lib"),
+      "@hooks": path.resolve(__dirname, "./client/hooks"),
+      "@providers": path.resolve(__dirname, "./client/providers"),
+      "@ui": path.resolve(__dirname, "./client/components/ui"),
+      "@workers": path.resolve(__dirname, "./workers")
+    }
+  }
 });
