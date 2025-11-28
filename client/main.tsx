829e93702ea85babb78ce8956d8934d1dbeb8c88
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-11-28 23:23:41 +0800

    added inter font support

--- client/main.tsx
+++ client/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "@client/globals.css";
+import "@fontsource-variable/inter";
 
 import { App } from "@components/app";
 import { SessionProvider } from "@providers/session";
