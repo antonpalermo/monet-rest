df92bc18fe6b48e354130d52136942f0de00850d
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-12-06 00:27:53 +0800

    init query client

--- client/main.tsx
+++ client/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "@client/globals.css";
 import "@fontsource-variable/inter";
@@ -7,10 +8,14 @@ import "@fontsource-variable/inter";
 import { App } from "@components/app";
 import { SessionProvider } from "@providers/session";
 
+const client = new QueryClient();
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <SessionProvider>
-      <App />
-    </SessionProvider>
+    <QueryClientProvider client={client}>
+      <SessionProvider>
+        <App />
+      </SessionProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
